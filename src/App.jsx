3c2dae4ba4959cbb237676cfc2fed67a786906f5
Vasquez8 <dv113409@gmail.com>
2025-03-13 02:31:09 -0600

    Se agrego el diseño a la claculadora

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,49 @@
-import { useState } from "react";
 import "./App.css";
+import milogo from "./img/mi-logo.png";
+import Boton from "./componentes/Boton.jsx";
+import Pantalla from "./componentes/Pantalla.jsx";
+import BotonClear from "./componentes/BotonClear.jsx";
 
 function App() {
-  return <div className="App">Calculadora Interactiva</div>;
+  return (
+    <div className="App">
+      <div className="my-logo-contenedor">
+        <img src={milogo} className="my-logo" alt="David Vasquez mi logo" />
+      </div>
+      <div className="contenedor-calculadora">
+        <div className="contenedor">
+          <Pantalla />
+        </div>
+        <div className="fila">
+          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
+          <Boton>+</Boton>
+        </div>
+        <div className="fila">
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
+        </div>
+        <div className="fila">
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+        </div>
+        <div className="fila">
+          <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
+        </div>
+        <div className="fila">
+          <BotonClear>CLEAR</BotonClear>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
